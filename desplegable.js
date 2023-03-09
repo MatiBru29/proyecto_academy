0672d9f44701b5839cfe620aa804959f06d0fbb0
MatiBru29 <127366620+MatiBru29@users.noreply.github.com>
2023-03-08 23:38:12 -0300

    Add files via upload

---
+++ desplegable.js
@@ -0,0 +1,8 @@
+function mostrarDatos() {
+  var datos = document.getElementById("datos");
+  if (datos.style.display === "none") {
+    datos.style.display = "block";
+  } else {
+    datos.style.display = "none";
+  }
+}
